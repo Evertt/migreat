7ff08ef1070657985240ea0da4c2fcd6badcc0e8
Evert van Brussel <ecvanbrussel@gmail.com>
2020-06-30 11:43:07 +0200

    Initial commit

---
+++ src/components/Header.svelte
@@ -0,0 +1,53 @@
+<header>
+  <a class="logo" href="/">
+    <img src="/img/logo.png" alt="MiGreat Logo" />
+  </a>
+  <div>
+    <a href="#" class="donate">Donate</a>
+    <a href="#" class="menu"></a>
+  </div>
+</header>
+
+<style>
+  header {
+    --height: 2rem;
+    padding: 1rem 0;
+    display: flex;
+    justify-content: space-between;
+  }
+
+  div {
+    display: flex;
+    align-items: center;
+  }
+
+  .logo {
+    --logo-height: 3rem;
+    display: inline-block;
+    height: var(--logo-height);
+  }
+
+  img {
+    height: var(--logo-height);
+  }
+
+  .donate {
+    border: none;
+    background: white;
+    padding: 0 2rem;
+    height: var(--height);
+    font-weight: bold;
+    margin-right: 1rem;
+    color: black;
+    text-decoration: none;
+    line-height: var(--height);
+  }
+
+  .menu {
+    border: none;
+    width: var(--height);
+    height: var(--height);
+    background-image: url(/img/menu.png);
+    background-size: cover;
+  }
+</style>
---
+++ src/pages/_layout.svelte
@@ -0,0 +1,26 @@
+<script>
+  import { writable } from 'svelte/store'
+  import Header from '/components/Header.svelte'
+  import { TabsTransition } from '@sveltech/routify/decorators'
+
+  const width = writable()
+</script>
+
+<div class="container">
+  <Header/>
+
+  <div bind:offsetWidth={$width}>
+    <slot decorator={TabsTransition} scoped={{ width }} />
+  </div>
+</div>
+
+<style>
+  :global(body) {
+    background-color: #FBBA00;
+  }
+
+  .container {
+    width: 800px;
+    margin: 0 auto;
+  }
+</style>
